599a8ba88ecff7113624e6f6b327ac893e97a1d4
Natsu <phamdatytb2006@gmail.com>
2026-07-14 17:04:02 +0700

    [SECURITY] feature/security Validate JWT Accsess Token

--- lifebalance-backend/identity-service/src/main/java/com/lifebalance/identity/config/SecurityConfig.java
+++ lifebalance-backend/identity-service/src/main/java/com/lifebalance/identity/config/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +27,23 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/health/**", "/actuator/info").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/*/status").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/login").permitAll()
+
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt());
+
+        return http.build();
+
     }
 
 }
