acd76159906de00225cfc373ba4a56ac73d31ac8
tadtn_be <tadtnwork@gmail.com>
2026-07-15 14:03:12 +0100

    [FIX] fix/identity-security-filter-chain remove duplicate identity security filter chain bean

--- lifebalance-backend/identity-service/src/main/java/com/lifebalance/identity/config/SecurityConfig.java
+++ lifebalance-backend/identity-service/src/main/java/com/lifebalance/identity/config/SecurityConfig.java
@@ -8,9 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -30,20 +28,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
-    }
-
-    @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(auth -> auth
-                        .pathMatchers("/login").permitAll()
-
-                        .anyExchange().authenticated())
-                .oauth2ResourceServer(oauth -> oauth.jwt());
-
-        return http.build();
-
     }
 
 }
