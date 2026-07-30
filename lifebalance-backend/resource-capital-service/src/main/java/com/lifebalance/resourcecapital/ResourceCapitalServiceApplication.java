94c654ef0f2fb15ad563dc703cfd10e809f41b03
tadtn_be <tadtnwork@gmail.com>
2026-07-30 02:03:07 +0100

    [FEATURE] feature/resource-capital-cycle-entity create resource capital service module

---
+++ lifebalance-backend/resource-capital-service/src/main/java/com/lifebalance/resourcecapital/ResourceCapitalServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lifebalance.resourcecapital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceCapitalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceCapitalServiceApplication.class, args);
+    }
+}
