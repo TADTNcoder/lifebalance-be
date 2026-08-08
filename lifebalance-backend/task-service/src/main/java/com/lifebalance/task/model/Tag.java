062f03942328d95d409ee9e40396e5709d9174be
tadtn_be <tadtnwork@gmail.com>
2026-08-08 16:37:42 +0100

    [DOMAIN] feature/lb-849-task-tag-entity Add task tag relationship helpers

--- lifebalance-backend/task-service/src/main/java/com/lifebalance/task/model/Tag.java
+++ lifebalance-backend/task-service/src/main/java/com/lifebalance/task/model/Tag.java
@@ -1,13 +1,17 @@
 package com.lifebalance.task.model;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -52,6 +56,10 @@ public class Tag extends BaseAuditableEntity {
     @Size(max = NAME_MAX_LENGTH)
     @Column(name = "name", nullable = false, length = NAME_MAX_LENGTH)
     private String name;
+
+    @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<TaskTag> taskTags = new HashSet<>();
 
     public boolean belongsTo(UUID userId) {
         return Objects.equals(this.userId, userId);
