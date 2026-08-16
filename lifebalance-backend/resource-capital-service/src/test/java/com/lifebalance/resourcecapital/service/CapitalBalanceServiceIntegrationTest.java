8e2672278f8f2dbee9c0533cffddb6cdaf6f4e60
tadtn_be <tadtnwork@gmail.com>
2026-08-16 11:02:18 +0700

    [TEST] feature/lb-1825-capital-adjustment-entity align resource capital integration tests

--- lifebalance-backend/resource-capital-service/src/test/java/com/lifebalance/resourcecapital/service/CapitalBalanceServiceIntegrationTest.java
+++ lifebalance-backend/resource-capital-service/src/test/java/com/lifebalance/resourcecapital/service/CapitalBalanceServiceIntegrationTest.java
@@ -20,6 +20,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -52,6 +53,9 @@ class CapitalBalanceServiceIntegrationTest {
 
     @Autowired
     private AllocationService allocationService;
+
+    @MockitoBean
+    private AllocationTargetValidator allocationTargetValidator;
 
     @Autowired
     private CapitalCycleRepository capitalCycleRepository;
